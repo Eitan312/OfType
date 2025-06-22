0350490d4bae7bb07386ce161694c307982df067
Eitan312 <eitanratner123@gmail.com>
2025-06-22 18:57:22 +0300

    Solved Question

---
+++ OfType/OfType.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+namespace OfType
+{
+    public class TypeFiftyNine : IOfType
+    {
+        public IEnumerable<T> GetOfType<T>(IEnumerable enumerable)
+        {
+            return enumerable.OfType<T>();
+        }
+
+        public IEnumerable<TOutput> GetOfType<TSource, TOutput>(IEnumerable<TSource> enumerable)
+        {
+            return enumerable.OfType<TOutput>();
+        }
+
+        public IEnumerable<TBase> OfBase<TBase, TDerived>(IEnumerable<TDerived> derivedItems) where TDerived : TBase
+        {
+            return derivedItems.OfType<TBase>();
+        }
+    }
+}
---
+++ OfType/Program.cs
@@ -0,0 +1,9 @@
+﻿using OfType;
+
+TypeFiftyNine fiftyNine = new TypeFiftyNine();
+IEnumerable<string> res = fiftyNine.GetOfType<string>(new object[] { "dasdsa", 4, "dsasdas" });
+Console.WriteLine(res.Count());
+foreach (string s in res)
+{
+    Console.WriteLine(s);
+}
